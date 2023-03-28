8192f3e617aa5b16c1a7365e13bf44bb60eef282
Alexandre Vasconcellos <avasconcellos204@gmail.com>
2023-03-28 20:04:04 -0300

    Swap config added. readme updated

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # linux-env-setup
 Simple script that helps to configure linux machines
+
+# how to configure
+
+Edit `linux-setup.conf` with your preferences.
+
+# running
+
+Execute with
+`./linux-setup.sh config-file.conf`
+
--- linux-setup.sh
+++ linux-setup.sh
@@ -10,28 +10,26 @@ get_config_value() {
     echo "$config_value"
 }
 
-# Function to check if a variable is not null and not empty
-function not_empty {
-    if [ -n "$1" ] && [ -n "${1// }" ]; then
-        return 1  # variable is not null and not empty
-    else
-        return 0  # variable is null or empty
-    fi
-}
-
-CONFIG_FILE="linux-setup.conf"
-
+CONFIG_FILE=$1
+if [ $# -gt 0 ]; then
+    echo "Config file is $1"
+else
+    echo "config file not set. Run as $0 config-file.conf"
+    exit 1
+fi
 
 # install apps
 echo "Installing basic apps"
 sudo apt remove konqueror
 sudo apt install -y flatpak gparted qemu-kvm virt-manager virtinst libvirt-clients \
-     bridge-utils libvirt-daemon-system 
+     bridge-utils libvirt-daemon-system  chromium unrar-free
 
 echo "Installing flatpak apps"
 flatpak install -y flathub com.visualstudio.code
 flatpak install -y flathub com.bitwarden.desktop
-flatpak install -y flathub org.chromium.Chromium
+
+## note: chrome on flatpak is not hardware accelerated
+#flatpak install -y flathub org.chromium.Chromium
 
 # Check if crontab exists for the current user
 if crontab -l &> /dev/null; then
@@ -45,7 +43,7 @@ EOF
 fi
 
 AUTODELETE_FILES_ON_DOWLOAD_AFTER_DAYS=$(get_config_value "$CONFIG_FILE" "AUTODELETE_FILES_ON_DOWLOAD_AFTER_DAYS")
-if not_empty "$AUTODELETE_FILES_ON_DOWLOAD_AFTER_DAYS"; then
+if [[ -n  "$AUTODELETE_FILES_ON_DOWLOAD_AFTER_DAYS" ]]; then
     sudo apt install -y trash-cli
     (crontab -l ; echo "0 0 * * 0 find ~/Downloads -depth -type f -mtime +$AUTODELETE_FILES_ON_DOWLOAD_AFTER_DAYS -exec trash {} \;") | crontab -
 else
@@ -53,7 +51,7 @@ else
 fi
 
 AUTOTRASH_MIN_FREE_MB=$(get_config_value "$CONFIG_FILE" "AUTOTRASH_MIN_FREE_MB")
-if not_empty "$AUTOTRASH_MIN_FREE_MB"; then
+if [[ -n  "$AUTOTRASH_MIN_FREE_MB" ]]; then
     pip install --user autotrash
     (crontab -l ; echo "0 0 * * 0 autotrash -t --min-free $AUTOTRASH_MIN_FREE_MB > ~/autotrash") | crontab -
 else
@@ -63,10 +61,30 @@ fi
 GIT_USER_EMAIL=$(get_config_value "$CONFIG_FILE" "GIT_USER_EMAIL")
 GIT_USER_FULL_NAME=$(get_config_value "$CONFIG_FILE" "GIT_USER_FULL_NAME")
 GIT_EDITOR=$(get_config_value "$CONFIG_FILE" "GIT_EDITOR")
-if not_empty "$GIT_USER_FULL_NAME" && not_empty "$GIT_USER_EMAIL" && not_empty "$GIT_EDITOR"; then
+if [[ -n  "$GIT_USER_FULL_NAME" ]] && [[ -n   "$GIT_USER_EMAIL" ]] && [[ -n  "$GIT_EDITOR" ]]; then
     git config --global core.editor $GIT_EDITOR
     git config --global user.name "$GIT_USER_FULL_NAME"
     git config --global user.email "$GIT_USER_EMAIL"
 else
     echo "not configuring git as configration is not set on $CONFIG_FILE"
 fi 
+
+        
+SWAP_FILE_SIZE=$(get_config_value "$CONFIG_FILE" "SWAP_FILE_SIZE")
+SWAP_FILE_PATH=$(get_config_value "$CONFIG_FILE" "SWAP_FILE_PATH")
+if [[ -n "$SWAP_FILE_SIZE" ]] && [[ -n "$SWAP_FILE_PATH" ]]; then
+    if [ -e $SWAP_FILE_PATH ]; then
+        echo "Swap file $SWAP_FILE_PATH already exists"
+    else    
+        echo "Creating swap file"
+        sudo cp /etc/fstab /etc/fstab.bak
+        sudo fallocate -l $SWAP_FILE_SIZE $SWAP_FILE_PATH
+        sudo chmod 600 $SWAP_FILE_PATH
+        sudo mkswap $SWAP_FILE_PATH
+        sudo swapon $SWAP_FILE_PATH
+        echo "$SWAP_FILE_PATH swap swap defaults 0 0" | sudo tee -a /etc/fstab
+    fi
+else
+    echo "not configuring swap file as SWAP_FILE_SIZE or SWAP_FILE_PATH are not set"
+    echo "SWAP_FILE_SIZE: $SWAP_FILE_SIZE, SWAP_FILE_PATH: $SWAP_FILE_PATH"
+fi
